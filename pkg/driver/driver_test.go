79585dcd4d12489256af66bce2b4793fd6506925
Matt Smith <mss@datera.io>
2018-09-24 15:24:25 -0700

    Adding driver tests for csi-sanity

--- pkg/driver/driver_test.go
+++ pkg/driver/driver_test.go
@@ -2,8 +2,40 @@ package driver
 
 import (
 	"testing"
+
+	// csi "github.com/container-storage-interface/spec/lib/go/csi/v0"
+	sanity "github.com/kubernetes-csi/csi-test/pkg/sanity"
+
+	udc "github.com/Datera/go-udc/pkg/udc"
 )
 
-func TestDriverBasic(t *testing.T) {
+const (
+	endpoint = "test-csi.sock"
+)
 
+func getDriver(t *testing.T) *Driver {
+	conf, err := udc.GetConfig()
+	if err != nil {
+		t.Fatal(err)
+	}
+	d, err := NewDateraDriver(endpoint, conf)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return d
+}
+
+func TestDriver(t *testing.T) {
+	go func() {
+		d := getDriver(t)
+		if err := d.Run(); err != nil {
+			t.Fatal(err)
+		}
+	}()
+	sc := &sanity.Config{
+		TargetPath:  "/mnt/my-publish",
+		StagingPath: "/mnt/my-staging",
+		Address:     "test-csi.sock",
+	}
+	sanity.Test(t, sc)
 }
