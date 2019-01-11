c2e511e9a439e7acc6d5ef7ddfc9aab62642989b
Matt Smith <mss@datera.io>
2019-01-11 14:16:11 -0600

    Adding bash script to download register and start the iscsi-recv service

---
+++ assets/setup_iscsi.sh
@@ -0,0 +1,88 @@
+#!/bin/bash -x
+
+IFILE="https://github.com/Datera/datera-csi/releases/download/v1.0.3/iscsi-recv.linux.x86-64"
+SFILE="/tmp/iscsi-recv"
+SCSI_SHA1="809e0816f6a294c68c07a71f7c7206f4ac165d9b"
+
+SCSI_SERVICE="[Unit]
+Description = iscsi-recv container to host iscsiadm adapter service
+
+[Service]
+ExecStart = /tmp/iscsi-recv -addr unix:///tmp/csi-iscsi.sock
+
+[Install]
+WantedBy = multi-user.target"
+
+SCSI_SERVICE_NAME="iscsi-recv.service"
+SCSI_SERVICE_FILE="/lib/systemd/system/${SCSI_SERVICE_NAME}"
+
+function check_external_dependencies()
+{
+    #Make sure these executables exist in PATH
+    local execs="curl sha1sum"
+    echo "[INFO] Dependency checking"
+
+    for bx in ${execs}
+    do
+            type -p "${bx}" &>/dev/null
+            if [ $? != 0 ] ; then
+                    echo "Error: Can't find  ${bx} in PATH=${PATH}"
+                    exit 1
+            fi
+    done
+}
+
+function get_and_start_iscsi_recv()
+{
+    echo "[INFO] Downloading iscsi-recv"
+    if ! curl -f -L --silent "${IFILE}" > "${SFILE}"
+    then
+        echo "[ERROR] Downloading iscsi-recv binary from ${IFILE}"
+        exit 1
+    fi
+
+    echo "[INFO] Verifying checksum"
+    local foundsum=$(sha1sum ${SFILE} | awk '{print $1}')
+    if [[ ${foundsum} != "${SCSI_SHA1}" ]]
+    then
+        echo "[ERROR] Checksum of downloaded iscsi-recv does not match [${foundsum}] != [${SCSI_SHA1}]"
+        exit 1
+    fi
+
+    echo "[INFO] Changing file permissions"
+    chmod +x "${SFILE}"
+
+    echo "[INFO] Registering iscsi-recv service"
+    echo "${SCSI_SERVICE}" > "${SCSI_SERVICE_FILE}"
+    if [ ! -e "${SCSI_SERVICE_FILE}" ]
+    then
+        echo "[ERROR] Failed to create '${SCSI_SERVICE_FILE}'.  Exiting"
+        exit 1
+    fi
+    systemctl enable "${SCSI_SERVICE_NAME}"
+    if [[ $? != 0 ]]
+    then
+        echo "[ERROR] Failed to register iscsi-recv service"
+        exit 1
+    fi
+
+    echo "[INFO] Starting iscsi-recv service"
+    systemctl start "${SCSI_SERVICE_NAME}"
+    if [[ $? != 0 ]]
+    then
+        echo "[ERROR] Failed to start iscsi-recv service"
+        exit 1
+    fi
+
+    sleep 1
+
+    echo "[INFO] Verifying service started correctly"
+    ps -ef | grep "${SFILE}" | grep -v "grep"
+    if [[ $? != 0 ]]
+    then
+        echo "[ERROR] Failed to verify iscsi-recv service is running"
+    fi
+}
+
+check_external_dependencies
+get_and_start_iscsi_recv
