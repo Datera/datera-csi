5b6ca660a526fd0b0c01e5dc56173164f792ea12
Matt Smith <mss@datera.io>
2019-01-31 15:12:09 -0800

    Adding log collect from remote hosts

--- assets/csi_log_collect.sh
+++ assets/csi_log_collect.sh
@@ -3,6 +3,7 @@
 KUBECTL="kubectl"
 POD_REGEX="csi-(provisioner|node)-"
 RSYNC="rsync://rts7.daterainc.com:/dumps/"
+USERNAME="root"
 
 function genstr()
 {
@@ -16,7 +17,7 @@ ARCHIVE=/tmp/csi-logs-$(hostname)-${SAVE_STR}.tar.gz
 function check_external_dependencies()
 {
     #Make sure these executables exist in PATH
-    local execs="${KUBECTL} grep awk tar rsync"
+    local execs="${KUBECTL} grep awk tar rsync sshpass"
     echo "[INFO] Dependency checking"
 
     for bx in ${execs}
@@ -54,15 +55,47 @@ function collect_logs()
 {
     echo "[INFO] Collecting CSI logs"
     mkdir -p ${SAVE_DIR}
+    # OS information
     cat /etc/*-release > ${SAVE_DIR}/os_release.txt
+    # Kubectl Version
     ${KUBECTL} version > ${SAVE_DIR}/kubectl_version.txt
+    # Local node messages log
+    mkdir -p ${SAVE_DIR}/$(hostname)
+    cat /var/log/messages > ${SAVE_DIR}/$(hostname)/messages
+    dmesg > ${SAVE_DIR}/$(hostname)/dmesg
+    # Find relevant pods
     local pods=$(${KUBECTL} get pods --namespace kube-system | grep -E "${POD_REGEX}" | awk '{print $1}')
+    # Collect pod logs
     for pod in ${pods}
     do
         echo "[INFO] Collecting for pod: ${pod}"
         collect_pod_logs ${pod}
     done
+}
+
+function collect_remote_logs() {
+    echo "[INFO] Collecting logs from remote nodes"
+    if [[ ${HOST_IPS} == "" ]]
+    then
+        HOST_IPS=$(kubectl describe nodes | grep InternalIP | awk '{print $2}' | xargs)
+    fi
+    if [[ ${OPT_R} == true ]]
+    then
+        mv ~/.ssh/known_hosts ~/.ssh/known_hosts.old
+    fi
+    echo "[INFO] HOST_IPS: ${HOST_IPS}"
+    for ip in ${HOST_IPS}
+    do
+        echo "[INFO] Collecting logs from ${ip}"
+        mkdir -p ${SAVE_DIR}/${ip}
+        sshpass -p "${PASSWORD}" scp ${USERNAME}@${ip}:/var/log/messages ${SAVE_DIR}/${ip}/messages
+        sshpass -p "${PASSWORD}" ssh -o "StrictHostKeyChecking=no" ${USERNAME}@${ip} dmesg > ${SAVE_DIR}/${ip}/dmesg
+    done
+}
+
+function create_archive() {
     echo "[INFO] Creating archive: ${ARCHIVE}"
+    # Tar archive
     tar cvfz ${ARCHIVE} -C ${SAVE_DIR} . > /dev/null 2>&1
     if [[ $? != 0 ]]
     then
@@ -91,16 +124,22 @@ This script will iterate through all relevant CSI pods and download their logs
 into a tarball archive located in /tmp/
 
 Usage: $0 [-k KUBECTL -p POD_REGEX -hs]
--h Print Help
--s Skip dependency check
--u Upload logs to /dumps
--k KUBECTL Use non-standard kubectl
--p POD_REGEX Regex (grep -E compatible) to match pods for log collection
+-h Print Help (optional)
+-s Skip dependency check (optional)
+-u Upload logs to /dumps (optional)
+-k KUBECTL Use non-standard kubectl (optional, default: kubectl)
+-p POD_REGEX Regex (optional, grep -E compatible) to match pods for log collection
+-n USERNAME Username for kubernetes nodes (default: root)
+-l PASSWORD Password for kubernetes nodes (optional unless collecting system logs)
+-b HOST_IPS Comma delimited list of ip addresses (optional, provide if log
+            collect can't determine them from kubectl describe nodes)
+-r Remove known_hosts file (backed up to known_hosts.old.  Use this if ssh into
+   remote nodes is failing
 "; exit 1;
 }
 
 OPT_S=false
-while getopts ":hsuk:p:" option
+while getopts ":hsurk:p:n:l:" option
 do
     case "${option}"
     in
@@ -111,6 +150,14 @@ do
         k) KUBECTL=${OPTARG}
           ;;
         p) POD_REGEX=${OPTARG}
+          ;;
+        n) USERNAME=${OPTARG}
+          ;;
+        l) PASSWORD=${OPTARG}
+          ;;
+        b) HOST_IPS=$(echo "${OPTARG}" | sed 's/,/ /g')
+          ;;
+        r) OPT_R=true
           ;;
         \?) echo "Invalid Option: -${OPTARG}" >&2; exit 1
           ;;
@@ -125,6 +172,13 @@ then
 fi
 
 collect_logs
+
+if [[ ${PASSWORD} != "" ]]
+then
+    collect_remote_logs
+fi
+
+create_archive
 
 if [[ ${OPT_U} == true ]]
 then
