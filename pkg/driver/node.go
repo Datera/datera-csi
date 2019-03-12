fa682d05eaad01bccd61bc75b509def508a246ac
Matt Smith <mss@datera.io>
2019-03-12 10:24:07 -0500

    Disabling ip_pool registration at attach-time

--- pkg/driver/node.go
+++ pkg/driver/node.go
@@ -46,19 +46,19 @@ func (d *Driver) NodeStageVolume(ctx context.Context, req *csi.NodeStageVolumeRe
 		return nil, status.Errorf(codes.Unknown, err.Error())
 	}
 
-	// Setup IpPool
-	if vol.Template == "" {
-		co.Debugf(ctxt, "Registering IP Pool: %s", (*md)["ip_pool"])
-		if ipp, err := d.dc.GetIpPoolFromName((*md)["ip_pool"]); err != nil {
-			return nil, status.Errorf(codes.NotFound, err.Error())
-		} else {
-			if err = vol.RegisterIpPool(ipp); err != nil {
-				return nil, status.Errorf(codes.Unknown, err.Error())
-			}
-		}
-	} else {
-		co.Debug(ctxt, "Skipping IP Pool registration due to Template")
-	}
+	// // Setup IpPool
+	// if vol.Template == "" {
+	// 	co.Debugf(ctxt, "Registering IP Pool: %s", (*md)["ip_pool"])
+	// 	if ipp, err := d.dc.GetIpPoolFromName((*md)["ip_pool"]); err != nil {
+	// 		return nil, status.Errorf(codes.NotFound, err.Error())
+	// 	} else {
+	// 		if err = vol.RegisterIpPool(ipp); err != nil {
+	// 			return nil, status.Errorf(codes.Unknown, err.Error())
+	// 		}
+	// 	}
+	// } else {
+	// 	co.Debug(ctxt, "Skipping IP Pool registration due to Template")
+	// }
 	// Online AI (to ensure targets are accessible)
 	if err = vol.Online(); err != nil {
 		return nil, status.Errorf(codes.Unknown, err.Error())
